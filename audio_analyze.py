80d765147cadae6f1eb00e01c2ff8a8c33789348
Shuang@DU02 <co.liang.ol@gmail.com>
2014-07-17 00:38:12 -0400

    Employ scikit-learn to train hmm

--- audio_analyze.py
+++ audio_analyze.py
@@ -6,7 +6,12 @@ import sqlite3
 import ast
 import datetime as dt
 import audio_features as af
-
+#import nltk : needs tagged token, which combine a basic token value with a tag.
+# refer to [http://docs.huihoo.com/nltk/0.9.5/guides/tag.html] for details
+import numpy as np
+from sklearn import hmm 
+#: removed since 0.17 release of scikit-learn
+#from hmmlearn import hmm
 # funf database table 
 TABLE_NAME = 'data'
  
@@ -19,10 +24,25 @@ def main():
     
     ## Fetch and extract data
     cur.execute("SELECT * FROM " + TABLE_NAME)
-    rec = cur.fetchone()
 
-    audio = af.AudioFeatures(rec) 
-    print audio.getTime(), audio.getMfccs()
+    train_seqs = []
+    #rec = cur.fetchone()
+    for rec in cur:
+        audio = af.AudioFeatures(rec) 
+        train_seqs.append(audio.getMfccs())
+    #print len(train_seqs)
+    ## HMM
+    # Use unsupervised Baum-Welch to train HMM for different locations/scenes
+    model = hmm.GaussianHMM(3, "full")
+    X = np.array(train_seqs)
+    X = np.rot90(X, 1)
+    X = np.flipud(X)
+    X = np.array([X])
+    print X[0].shape
+    model.fit(X)
+
+    print 'Train finished'
+
 
 if __name__ == "__main__":
     main()
